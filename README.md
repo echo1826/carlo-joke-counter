65fa29504f85bb75fb07bd6e0ba1470ed48b9e63
Ethan C <ethan.cho1826@gmail.com>
2022-01-10 21:27:20 -0600

    welcome message functionality on member add

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # zen-bot
+
+Zen-bot is a discord bot that I am working on as a personal project. It uses the discord.js package to make the bot and give it functionality. As of now it only has a chat greeting functionality.
+
+# In Development
+- Music playback
+- Spotify API integration
+- Rock Paper Scissors
+
+# Technologies
+- [discord.js](https://discord.js.org)
--- index.js
+++ index.js
@@ -6,12 +6,27 @@ const botToken = process.env.BOT_TOKEN;
 const client = new Discord.Client({
     intents: [
         'GUILDS',
-        'GUILD_MESSAGES'
+        'GUILD_MESSAGES',
+        'GUILD_MEMBERS'
     ]
 });
 
 client.on('ready', () => {
+    // Username for the bot
     console.log(`Logged in as ${client.user.tag}`);
 });
+
+client.on('messageCreate', (message) => {
+    if(message.content === 'hi') {
+        message.reply("Hello World");
+    }
+});
+
+const welcomeChannel = '930295798665723934';
+
+client.on('guildMemberAdd', (member) => {
+    console.log(member);
+    member.guild.channels.cache.get(welcomeChannel).send(`Welcome ${member.user.username}`);
+})
 
 client.login(botToken);
