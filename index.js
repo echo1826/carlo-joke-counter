4b7a3d768311bfa637c135606284693b3fc25d90
Ethan C <ethan.cho1826@gmail.com>
2022-01-10 20:53:10 -0600

    initial commit and instantiating the bot

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+
+const botToken = process.env.BOT_TOKEN;
+
+const client = new Discord.Client({
+    intents: [
+        'GUILDS',
+        'GUILD_MESSAGES'
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(botToken);
